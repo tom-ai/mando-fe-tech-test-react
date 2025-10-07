7d69fb07e5eaad0e33b8d8beabc71ac61e804dd8
Tom Harris <tomharris3@me.com>
2025-10-07 14:19:34 +0100

    create initial page title component

---
+++ src/components/PageTItle.tsx
@@ -0,0 +1,13 @@
+// make some proptypes
+// make some data and export
+
+export type PageTitleProps = {
+  h1: string
+  description: string
+  image: string
+  imageAlt: string
+}
+
+export default function PageTitle() {
+  return <section></section>
+}
